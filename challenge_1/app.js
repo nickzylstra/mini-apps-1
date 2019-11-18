2af6c4a90c0851efba1de96563ba00045bc462a8
nickzylstra <nick.zylstra@gmail.com>
2019-11-18 11:20:37 -0800

    Updates c1, adds basic grid setup

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,20 @@
+// create empty game grid
+const createGrid = () => {
+  const grid = document.getElementById('grid');
+  for (let i = 0; i < 3; i += 1) {
+    const row = document.createElement('tr');
+    row.setAttribute('id', `r${i}`);
 
+    for (let j = 0; j < 3; j += 1) {
+      const cell = document.createElement('td');
+      cell.setAttribute('id', `r${i}c${j}`);
+      cell.setAttribute('class', 'cell');
+      cell.innerHTML = '_';
+      row.appendChild(cell);
+    }
 
+    grid.appendChild(row);
+  }
+};
+
+createGrid();
