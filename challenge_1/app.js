10ee0989f3b86647da62bc45cb4e016ed90df1ea
nickzylstra <nick.zylstra@gmail.com>
2019-11-18 11:57:29 -0800

    Updates c1, starts moveListener and ds for game

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,4 +1,23 @@
-// create empty game grid
+class Game {
+  constructor() {
+    this.turn = 0;
+    const emptyGrid = [];
+    for (let i = 0; i < 3; i += 1) {
+      emptyGrid.push([]);
+      for (let j = 0; j < 3; j += 1) {
+        emptyGrid[i].push(false);
+      }
+    }
+
+    this.grid = emptyGrid;
+  }
+
+  addMove() {
+
+  }
+}
+
+// creates empty game grid
 const createGrid = () => {
   const grid = document.getElementById('grid');
   for (let i = 0; i < 3; i += 1) {
@@ -15,14 +34,19 @@ const createGrid = () => {
 
     grid.appendChild(row);
   }
+
+  // click handler for boxes
+  const gridClickHandler = (e) => {
+
+  };
+
+  grid.addEventListener('click', gridClickHandler);
 };
 
-createGrid();
-
+// starts new game
 const resetGame = () => {
   const grid = document.getElementById('grid');
   grid.innerHTML = '';
   createGrid();
 };
-
 resetGame();
