7e122805a4881d0bca9b6bec8ed72868b907b730
nickzylstra <nick.zylstra@gmail.com>
2019-11-18 14:11:42 -0800

    Updates c1, refactors to mvc organization

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,74 +1,104 @@
+/* eslint-disable no-use-before-define */
 /* eslint-disable no-underscore-dangle */
-// DS for game
-let currentGame;
-class Game {
-  constructor() {
-    this._turn = 1;
-    this._size = 3;
+(() => {
+  // MODEL
+  // -------------------------------------------------------
+  // DS for game
+  let currentGame;
+  class Game {
+    constructor() {
+      this._turn = 1;
+      this._size = 3;
+      this._openMoveCellChar = '_';
 
-    const emptyGrid = [];
-    for (let i = 0; i < this._size; i += 1) {
-      emptyGrid.push([]);
-      for (let j = 0; j < this._size; j += 1) {
-        emptyGrid[i].push('_');
+      const emptyGrid = [];
+      for (let i = 0; i < this._size; i += 1) {
+        emptyGrid.push([]);
+        for (let j = 0; j < this._size; j += 1) {
+          emptyGrid[i].push(this._openMoveCellChar);
+        }
       }
-    }
-    this._grid = emptyGrid;
-  }
-
-  getSize() {
-    return this._size;
-  }
-
-  getBoxValue(row, col) {
-    return this._grid[row][col];
-  }
-
-  addMove(cell) {
-    // if (!this.grid[row][col]) {
-    // }
-  }
-}
-
-// click handler for moves
-const moveClickHandler = (e) => {
-  const clickedCell = e.target;
-  const row = clickedCell.getAttribute('id')[1];
-  const col = clickedCell.getAttribute('id')[3];
-
-  debugger;
-};
-
-
-// renders game grid
-const renderGrid = () => {
-  const size = currentGame.getSize();
-  const grid = document.getElementById('grid');
-  for (let i = 0; i < size; i += 1) {
-    const row = document.createElement('tr');
-    row.setAttribute('id', `r${i}`);
-
-    for (let j = 0; j < size; j += 1) {
-      const cell = document.createElement('td');
-      cell.setAttribute('id', `r${i}c${j}`);
-      cell.setAttribute('class', 'cell');
-      cell.innerHTML = currentGame.getBoxValue(i, j);
-      row.appendChild(cell);
+      this._grid = emptyGrid;
     }
 
-    grid.appendChild(row);
+    getSize() {
+      return this._size;
+    }
+
+    getCellValue(row, col) {
+      return this._grid[row][col];
+    }
+
+    isCellOpen(row, col) {
+      return this.getCellValue(row, col) === this._openMoveCellChar;
+    }
+
+    addMove(cell) {
+      // if (!this.grid[row][col]) {
+      // }
+    }
   }
 
-  grid.addEventListener('click', moveClickHandler);
-};
+  // resets game
+  const resetGame = () => {
+    currentGame = new Game();
+    renderGrid();
+  };
 
-// starts new game
-const resetGame = () => {
-  currentGame = new Game();
-  const grid = document.getElementById('grid');
-  grid.innerHTML = '';
-  renderGrid();
-};
+  // VIEWS
+  // -------------------------------------------------------
+  // renders game grid
+  const renderGrid = () => {
+    const grid = document.getElementById('grid');
+    grid.innerHTML = '';
 
-// initializes app
-resetGame();
+    const size = currentGame.getSize();
+    for (let i = 0; i < size; i += 1) {
+      const row = document.createElement('tr');
+      row.setAttribute('id', `r${i}`);
+
+      for (let j = 0; j < size; j += 1) {
+        const cell = document.createElement('td');
+        cell.setAttribute('id', `r${i}c${j}`);
+        cell.setAttribute('class', 'cell');
+        cell.innerHTML = currentGame.getCellValue(i, j);
+        row.appendChild(cell);
+      }
+
+      grid.appendChild(row);
+    }
+  };
+
+  // CONTROLLERS
+  // -------------------------------------------------------
+  // click handler for moves
+  const handleMoveClick = (e) => {
+    const clickedCell = e.target;
+    const row = clickedCell.getAttribute('id')[1];
+    const col = clickedCell.getAttribute('id')[3];
+    if (currentGame.isCellOpen(row, col)) {
+      currentGame.addMove(row, col);
+    }
+  };
+
+  // click handler for new game button
+  const handleNewGameButtonClick = () => {
+    resetGame();
+  };
+
+  const initializeControllers = () => {
+    const grid = document.getElementById('grid');
+    grid.addEventListener('click', handleMoveClick);
+
+    const newGameButton = document.getElementById('new-game-btn');
+    newGameButton.addEventListener('click', handleNewGameButtonClick);
+  };
+
+  // APP
+  // -------------------------------------------------------
+  // initializes app
+  (function initializeApp() {
+    resetGame();
+    initializeControllers();
+  }());
+})();
