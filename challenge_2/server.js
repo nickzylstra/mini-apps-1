3ecff2812b8ad691576866b5a1cc5a87ac8b3461
nickzylstra <nick.zylstra@gmail.com>
2019-11-19 12:34:46 -0800

    Updates c2, env config to match repo

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -4,6 +4,6 @@ const app = express();
 const port = 3000;
 
 // app.get('/', (req, res) => res.send('heyo'));
-app.use('/', express.static('dist'));
+app.use('/', express.static('client'));
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`server listening on ${port}`));
--- challenge_2/src/index.js
+++ challenge_2/src/index.js
@@ -1,5 +1,8 @@
-const app = document.getElementById('app');
-console.log(app);
-const div = document.createElement('div');
-div.innerHTML = 'hiya from index.js';
-app.appendChild(div);
+window.addEventListener('DOMContentLoaded', () => {
+  const app = document.getElementById('app');
+  (function test() {
+    const div = document.createElement('div');
+    div.innerHTML = 'hiya from index.js';
+    app.appendChild(div);
+  }());
+});
