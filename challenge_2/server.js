1b875646d24821c8f7274247e483ed7bf57edc0c
nickzylstra <nick.zylstra@gmail.com>
2019-11-19 19:31:00 -0800

    Updates c2, more cleanup

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -5,14 +5,14 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// eslint-disable-next-line max-len
-/* The server must flatten the JSON hierarchy, mapping each item/object in the JSON to a single line of CSV report (see included sample output), where the keys of the JSON objects will be the columns of the CSV report. */
-// eslint-disable-next-line max-len
-/* You may assume the JSON data has a regular structure and hierarchy (see included sample file). In other words, all sibling records at a particular level of the hierarchy will have the same set of properties, but child objects might not contain the same properties. In all cases, every property you encounter must be present in the final CSV output.
-
-You may also assume that child records in the JSON will always be in a property called `children`.
- */
 const jsonToCSV = (json) => {
+  // eslint-disable-next-line max-len
+  /* The server must flatten the JSON hierarchy, mapping each item/object in the JSON to a single line of CSV report (see included sample output), where the keys of the JSON objects will be the columns of the CSV report. */
+  // eslint-disable-next-line max-len
+  /* You may assume the JSON data has a regular structure and hierarchy (see included sample file). In other words, all sibling records at a particular level of the hierarchy will have the same set of properties, but child objects might not contain the same properties. In all cases, every property you encounter must be present in the final CSV output.
+
+  You may also assume that child records in the JSON will always be in a property called `children`.
+   */
   const parsed = JSON.parse(json);
 
   const flattenObj = (obj, flatObj = { objCount: 0 }) => {
@@ -65,12 +65,12 @@ app.use('/', express.static('client'));
 app.listen(port, () => console.log(`server listening on ${port}`));
 
 app.post('/', express.urlencoded({ extended: true }), (req, res, next) => {
-  const csv = jsonToCSV(req.body.jsoninput);
   fs.readFile(path.join(__dirname, 'client', 'index.html'), (err, form) => {
     if (err) {
       console(err);
       next();
     }
+    const csv = jsonToCSV(req.body.jsoninput);
     const page = form + csv;
     res.status(200).send(page);
   });
