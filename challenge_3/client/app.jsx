d969557c8c07479435336f90f55008bbd35793a5
nickzylstra <nick.zylstra@gmail.com>
2019-11-20 14:36:14 -0800

    Ups c3, cleans up react for dynamic rendering

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-use-before-define */
 /* eslint-disable max-classes-per-file */
 /* eslint-disable react/react-in-jsx-scope */
@@ -6,35 +7,43 @@
 class App extends React.Component {
   constructor() {
     super();
-    this.curComp = 'Home';
-    this.components = {
-      Home,
+    this.state = {
+      curComp: 0,
+      components: [
+        Home,
+        F1,
+      ],
     };
+    this.showNextComp = this.showNextComp.bind(this);
+  }
+
+  showNextComp() {
+    let { curComp } = this.state;
+    this.setState({
+      curComp: curComp += 1,
+    });
   }
 
   render() {
-    const Comp = this.components[this.curComp];
+    const { curComp, components } = this.state;
+    const Comp = components[curComp];
     return (
-      <Comp />
+      <Comp showNextComp={this.showNextComp} />
     );
   }
 }
 
-// eslint-disable-next-line no-undef
-class Home extends React.Component {
-  handleCheckout() {
+const Home = ({ showNextComp }) => (
+  <div>
+    <button type="button" onClick={showNextComp}>Checkout</button>
+  </div>
+);
 
-  }
-
-  render() {
-    return (
-      <div>
-        <button type="button" onClick={this.handleCheckout()}>Checkout</button>
-      </div>
-    );
-  }
-}
-
+const F1 = ({ showNextComp }) => (
+  <div>
+    hi
+  </div>
+);
 
 // eslint-disable-next-line no-undef
 ReactDOM.render(<App />, app);
