a48e5e8852643d2db4b1020f1e0cbd5d6e399b6c
nickzylstra <nick.zylstra@gmail.com>
2019-11-20 12:44:31 -0800

    Ups c3, page renders react

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,3 +1,20 @@
-import react from 'react';
-import reactDOM from 'react-dom';
+/* eslint-disable react/react-in-jsx-scope */
+// import React from '../public/react.development';
+// import ReactDOM from '../public/react-dom.development';
 
+// eslint-disable-next-line no-undef
+class App extends React.Component {
+  constructor() {
+    super();
+    this.test = '';
+  }
+
+  render() {
+    return (
+      <div>hi</div>
+    );
+  }
+}
+
+// eslint-disable-next-line no-undef
+ReactDOM.render(<App />, app);
