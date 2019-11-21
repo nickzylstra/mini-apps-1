a866fd26fff22ac26740734bb18734d3429d9a93
nickzylstra <nick.zylstra@gmail.com>
2019-11-21 15:17:05 -0800

    Ups c4, finish env config

--- challenge_4/server/server.js
+++ challenge_4/server/server.js
@@ -1,8 +1,9 @@
+/* eslint-disable no-console */
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, '..', 'public')));
 app.listen(port, () => console.log(`listening on port ${port}`));
+app.use(express.static(path.join(__dirname, '..', 'public')));
