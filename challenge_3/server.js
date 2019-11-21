badd035ac8a3acaea4ad38462eeac71a1d23cc62
nickzylstra <nick.zylstra@gmail.com>
2019-11-20 16:43:13 -0800

    Ups c3, server accepts post request

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-console */
 const express = require('express');
 const path = require('path');
+const bodyparser = require('body-parser');
 
 const app = express();
 const port = 3000;
@@ -7,7 +9,8 @@ const port = 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyparser.json());
 
-// app.post('/f1', express.bodyParser(), (req, res, next) => {
-//   debugger;
-// });
+app.post('/f1', (req, res, next) => {
+  debugger;
+});
