6a489eb763ba554ee5eae353b5f8495394711a86
nickzylstra <nick.zylstra@gmail.com>
2019-11-20 17:27:53 -0800

    Ups c3, moves server into folder

--- challenge_3/server/server.js
+++ challenge_3/server/server.js
@@ -2,14 +2,24 @@
 const express = require('express');
 const path = require('path');
 const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3000;
-
 app.listen(port, () => console.log(`listening on port ${port}`));
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(bodyparser.json());
+
+mongoose.connect('mongodb://localhost/my_database', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then((result) => {
+    debugger;
+    console.log(`mongoose connected ${result}`);
+  }, (err) => {
+    console.log(`mongoose failed to connect, \n ${err}`);
+  });
 
 app.post('/Home', (req, res, next) => {
   // TODO replace userId with cookie
@@ -19,7 +29,6 @@ app.post('/Home', (req, res, next) => {
 });
 
 app.post('/F1', (req, res, next) => {
-  // TODO replace userId with cookie
   console.log('F1 submitted');
   const data = { userId: '00045' };
   res.end(JSON.stringify(data));
