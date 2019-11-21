72e9090b5f15bbf8fab48bcaa8eb78fc6ccaa354
nickzylstra <nick.zylstra@gmail.com>
2019-11-20 18:24:49 -0800

    Ups c3, starts db schema

--- challenge_3/server/model.js
+++ challenge_3/server/model.js
@@ -1,6 +1,9 @@
 /* eslint-disable no-console */
 const mongoose = require('mongoose');
 
+const { Schema } = mongoose;
+const { ObjectId } = Schema;
+
 mongoose.connect('mongodb://localhost/my_database', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -13,3 +16,14 @@ mongoose.connect('mongodb://localhost/my_database', {
   }, (err) => {
     console.log(`mongoose failed to connect, \n ${err}`);
   });
+
+const Order = new Schema({
+  userId: ObjectId,
+  name: String,
+  email: String,
+  // TODO - hash password
+  password: String,
+});
+
+const Model = mongoose.model('Order', Order);
+debugger;
