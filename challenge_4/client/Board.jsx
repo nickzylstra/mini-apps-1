47233c2caebfb703f1923cbbe1adeb1388327718
nickzylstra <nick.zylstra@gmail.com>
2019-11-21 16:11:37 -0800

    Ups c4, adds styles

--- challenge_4/client/Board.jsx
+++ challenge_4/client/Board.jsx
@@ -6,21 +6,57 @@ class Board extends React.Component {
     const width = 7;
     const height = 6;
     const grid = [];
-    for (let i = 0; i < height; i += 1) {
+    for (let i = 0; i < width; i += 1) {
       grid.push([]);
-      for (let j = 0; j < width; j += 1) {
+      for (let j = 0; j < height; j += 1) {
         grid[i].push(false);
       }
     }
     this.state = {
       grid,
+      height,
+      width,
     };
   }
 
+  addMove(col, player) {
+    const { grid, height } = this.state;
+    let moveAdded = false;
+    const colMoves = grid[col];
+
+    let row = 0;
+    while (row < height) {
+      if (!colMoves[row]) {
+        colMoves[row] = player;
+        moveAdded = true;
+        break;
+      }
+      row += 1;
+    }
+
+    return moveAdded;
+  }
+
   render() {
+    const { width, grid, height } = this.state;
     return (
       <div>
-        hi
+        <table>
+          <thead>
+            <tr>
+              <th colSpan={width}>Make a move</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              // for (let i = 0; i < height; i += 1) {
+              //   <tr>
+
+              //   </tr>
+              // }
+            }
+          </tbody>
+        </table>
       </div>
     );
   }
