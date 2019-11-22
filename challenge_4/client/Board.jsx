a3c70b67e644cc290f812667cf167a7273045051
nickzylstra <nick.zylstra@gmail.com>
2019-11-21 16:42:41 -0800

    Ups c4, renders board dynamically

--- challenge_4/client/Board.jsx
+++ challenge_4/client/Board.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
 
 class Board extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     const width = 7;
     const height = 6;
     const grid = [];
-    for (let i = 0; i < width; i += 1) {
+    const openVal = 'open';
+    for (let i = 0; i < height; i += 1) {
       grid.push([]);
-      for (let j = 0; j < height; j += 1) {
+      for (let j = 0; j < width; j += 1) {
         grid[i].push(false);
       }
     }
     this.state = {
       grid,
+      openVal,
       height,
       width,
     };
   }
 
   addMove(col, player) {
+    // UPDATE
     const { grid, height } = this.state;
     let moveAdded = false;
     const colMoves = grid[col];
@@ -37,24 +40,34 @@ class Board extends React.Component {
     return moveAdded;
   }
 
+  // eslint-disable-next-line class-methods-use-this
+  renderRow(row) {
+    return row.map((cell, idx) => (
+      <td className="cell">{cell}</td>
+    ));
+  }
+
+  renderGrid() {
+    const { grid } = this.state;
+    return grid.map((row, idx) => (
+      <tr>
+        {this.renderRow(row)}
+      </tr>
+    ));
+  }
+
   render() {
     const { width, grid, height } = this.state;
     return (
       <div>
-        <table>
+        <table className="table">
           <thead>
             <tr>
-              <th colSpan={width}>Make a move</th>
+              <th>Make a move</th>
             </tr>
           </thead>
           <tbody>
-            {
-              // for (let i = 0; i < height; i += 1) {
-              //   <tr>
-
-              //   </tr>
-              // }
-            }
+            {this.renderGrid()}
           </tbody>
         </table>
       </div>
